5c0c2fc73b95f86c9ef3ef9a91c91973e956162f
Luke Chen <luke.chen.2010@gmail.com>
2020-05-22 15:48:34 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -7,7 +7,7 @@ class Application
     req = Rack::Request.new(env)
  
     if req.path.match(/items/)
-      search_term = req.params["q"]
+      item = req.path.split("/songs/").last
     else
       resp.status = 404
       resp.write "Route not found"
