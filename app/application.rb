0f33bb336655f05baeb4a1ff167b346ea4cc284a
Luke Chen <luke.chen.2010@gmail.com>
2020-05-22 15:53:45 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -7,7 +7,7 @@ class Application
     req = Rack::Request.new(env)
  
     if req.path.match(/items/)
-      item = req.path.split("/items/").last
+      item_name = req.path.split("/items/").last
       
 
       end
