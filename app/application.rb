aee682c197e23e89967763dc43e826b285c0ee88
Luke Chen <luke.chen.2010@gmail.com>
2020-05-22 15:46:47 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,11 +1,13 @@
 class Application
+  
+  @@item
  
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
  
     if req.path.match(/items/)
-      
+      resp.write @@item.price
     else
       resp.status = 404
       resp.write "Route not found"
